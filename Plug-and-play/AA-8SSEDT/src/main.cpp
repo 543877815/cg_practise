a1dc1bea85f15eaf69ff8b48db159c0d3fa6e1f8
543877815 <543877815@qq.com>
2025-08-03 21:12:31 +0800

    SDF算法修复

--- Plug-and-play/AA-8SSEDT/src/main.cpp
+++ Plug-and-play/AA-8SSEDT/src/main.cpp
@@ -1,196 +1,117 @@
-#include <iostream>
-#include <vector>
-
+#include<iostream>
 #define STB_IMAGE_IMPLEMENTATION
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 
 #include "stb/stb_image.h"
 #include "stb/stb_image_write.h"
+#include <queue>
+#include <utility>
 #include "main.h"
 
-
-void compare(std::vector<std::vector<double>>& data,
-	std::vector<std::vector<Point>>& grid, int& height, int& width,
-	int row, int col, int offsetx, int offsety, bool& changed) {
-
-	double epsilon = 1e-3;
-	if (grid[row][col].dist <= 0)return;
-	int new_row = row + offsetx, new_col = col + offsety;
-	if (isValid(new_row, new_col, height, width)) {
-		int cdistx = grid[new_row][new_col].distx;
-		int cdisty = grid[new_row][new_col].disty;
-		int newdistx = cdistx + offsetx;
-		int newdisty = cdisty + offsety;
-		int closestx = new_row - cdistx;
-		int closesty = new_col - cdisty;
-
-		double newdist = distaa3(data, grid, closestx, closesty, newdistx, newdisty);
-		double olddist = grid[row][col].dist;
-
-		if (newdist < olddist - epsilon) {
-			grid[row][col].dist = newdist;
-			grid[row][col].distx = newdistx;
-			grid[row][col].disty = newdisty;
-			changed = true;
-		}
-	}
-
+void setColor(unsigned char* data, const int& c, const int& w, int row, int col, int idx, unsigned char val) {
+	data[c * w * row + col * c + idx] = val;
 }
 
-// 1. http://www.codersnotes.com/notes/signed-distance-fields/
-// 2. https://github.com/Lisapple/8SSEDT
-// 3. https://github.com/OpenGLInsights/OpenGLInsightsCode/tree/master/Chapter%2012%202D%20Shape%20Rendering%20by%20Distance%20Fields
-void generateSDF(
-	std::vector<std::vector<double>>& data,
-	std::vector<std::vector<Point>>& grid,
-	int& height, int& width) {
-	bool changed = false;
-
-	for (int row = 0; row < height; row++) {
-		for (int col = 0; col < width; col++) {
-			grid[row][col].distx = 0.0;
-			grid[row][col].disty = 0.0;
-			if (data[row][col] <= 0.0) {
-				grid[row][col].dist = 100000.0;
-			}
-			else if (data[row][col] < 1.0) {
-				grid[row][col].dist = edgedf(grid[row][col].gx, grid[row][col].gy, data[row][col]);
-			}
-			else {
-				grid[row][col].dist = 0.0;
-			}
-		}
-	}
-
-
-	int count = 0;
-	// Pass1
-	do {
-		changed = false;
-		for (int row = 0; row < height; row++) {
-			for (int col = 0; col < width; col++) {
-				compare(data, grid, height, width, row, col, -1, -1, changed);
-				compare(data, grid, height, width, row, col, -1, 0, changed);
-				compare(data, grid, height, width, row, col, -1, 1, changed);
-				compare(data, grid, height, width, row, col, 0, -1, changed);
-			}
-			for (int col = height - 1; col >= 0; col--) {
-				compare(data, grid, height, width, row, col, 0, 1, changed);
-			}
-		}
-		// Pass2
-		for (int row = height - 1; row >= 0; row--) {
-			for (int col = width - 1; col >= 0; col--) {
-				compare(data, grid, height, width, row, col, 0, 1, changed);
-				compare(data, grid, height, width, row, col, 1, -1, changed);
-				compare(data, grid, height, width, row, col, 1, 0, changed);
-				compare(data, grid, height, width, row, col, 1, 1, changed);
-			}
-
-			for (int col = 0; col < width; col++) {
-				compare(data, grid, height, width, row, col, 0, -1, changed);
-			}
-		}
-	} while (changed);
-
+unsigned char getColor(unsigned char* data, int c, int w, int row, int col, int idx) {
+	return data[c * w * row + col * c + idx];
 }
 
+int main() {
+	int w, h, c;
+	unsigned char* img = stbi_load("../resources/imgs/flipper128.png", &w, &h, &c, 0);
+	int rawChannel = 0;  // 3是alpha通道
+	int outputChannel = 4;
 
-void make_distance_map(unsigned char* img, int& width, int& height, int& nrChannels, int dataChannel, unsigned char* out, int outChannel)
-{
-	std::vector<std::vector<double>> data(height, std::vector<double>(width));
-	std::vector<std::vector<Point>> outside(height, std::vector<Point>(width));
-	std::vector<std::vector<Point>> inside(height, std::vector<Point>(width));
+	unsigned char* output = new unsigned char[w * h * outputChannel];
 
-	// Convert img into double (data)
+	double max_distance = 0;
+
+	double MAX = std::max(h, w) * 2;
+
+	std::vector<std::vector<Point>> inside(h, std::vector<Point>(w, { 0, 0, 0 }));
+	std::vector<std::vector<Point>> outside(h, std::vector<Point>(w, { 0, 0, 0 }));
+	std::vector<std::vector<double>> dist(h, std::vector<double>(w));
+
+	// convert img into dobule data
+	std::vector<std::vector<double>> data(h, std::vector<double>(w));
 	double img_min = 255, img_max = -255;
-	for (int row = 0; row < height; row++) {
-		for (int col = 0; col < width; col++) {
-			double v = getColor(img, width, nrChannels, row, col, dataChannel);
+	for (int row = 0; row < h; row++) {
+		for (int col = 0; col < w; col++) {
+			double v = static_cast<double>(getColor(img, c, w, row, col, rawChannel));
+			data[row][col] = v;
 			if (v > img_max) img_max = v;
 			if (v < img_min) img_min = v;
-
 		}
 	}
 
 	// Rescale image levels between 0 and 1
 	double img_offset = (img_min < 128.0) ? img_min : 0;
 	double img_range = img_max - img_min;
-	if (img_range == 0.0) img_range = 255.0; // Failsafe for constant image
-
-	for (int row = 0; row < height; row++) {
-		for (int col = 0; col < width; col++) {
-			int val = getColor(img, width, nrChannels, row, col, dataChannel);
-			data[row][col] = (val - img_offset) / img_range;
+	if (img_range == 0.0) img_range = 255.0; // safe for constant image;
+	for (int row = 0; row < h; row++) {
+		for (int col = 0; col < w; col++) {
+			double v = static_cast<double>(getColor(img, c, w, row, col, rawChannel));
+			data[row][col] = (v - img_offset) / img_range;
 		}
 	}
 
-	computegradient(data, outside, height, width);
-	generateSDF(data, outside, height, width);
-	for (int row = 0; row < height; row++) {
-		for (int col = 0; col < width; col++) {
-			if (outside[row][col].dist < 0.0) {
+	// Transform background (outside contour, in areas of 0's)
+	std::vector<std::vector<double>> gx(h, std::vector<double>(w, 0));
+	std::vector<std::vector<double>> gy(h, std::vector<double>(w, 0));
+	computeGradient(data, w, h, gx, gy);
+	edtaa3(data, gx, gy, outside, w, h);
+	for (int row = 0; row < h; row++) {
+		for (int col = 0; col < w; col++) {
+			if (outside[row][col].dist < 0.0)
 				outside[row][col].dist = 0.0;
-			}
 		}
 	}
 
-
-	for (int row = 0; row < height; row++) {
-		for (int col = 0; col < width; col++) {
+	for (int row = 0; row < h; row++) {
+		for (int col = 0; col < w; col++) {
 			data[row][col] = 1.0 - data[row][col];
 		}
 	}
-
-	computegradient(data, inside, height, width);
-	generateSDF(data, inside, height, width);
-	for (int row = 0; row < height; row++) {
-		for (int col = 0; col < width; col++) {
-			if (inside[row][col].dist < 0.0) {
+	computeGradient(data, w, h, gx, gy);
+	edtaa3(data, gx, gy, inside, w, h);
+	for (int row = 0; row < h; row++) {
+		for (int col = 0; col < w; col++) {
+			if (inside[row][col].dist < 0.0)
 				inside[row][col].dist = 0.0;
-			}
 		}
 	}
 
+	double min_dist = INT_MAX;
+	double max_dist = INT_MIN;
+	for (int row = 0; row < h; row++) {
+		for (int col = 0; col < w; col++) {
+			double dist1 = inside[row][col].dist2();
+			double dist2 = outside[row][col].dist2();
+			double dist = dist2 - dist1;
+			min_dist = std::min(min_dist, dist);
+			max_dist = std::max(max_dist, dist);
+		}
+	}
 
-	for (int row = 0; row < height; row++) {
-		for (int col = 0; col < width; col++) {
-			double dist = outside[row][col].dist - inside[row][col].dist;
-			dist = 128.0 + dist;
+	std::vector<std::vector<double>> SDF(h, std::vector<double>(w));
+	std::cout << "min_dist: " << min_dist << " ,max_dist: " << max_dist;
+	for (int row = 0; row < h; row++) {
+		for (int col = 0; col < w; col++) {
+			double dist1 = inside[row][col].dist;
+			double dist2 = outside[row][col].dist;
+			double dist = dist1 - dist2;
+			SDF[row][col] = dist;
+			dist = dist + 128.0;
 			if (dist < 0.0) dist = 0.0;
 			if (dist >= 256.0) dist = 255.999;
-
-			setColor(out, width, outChannel, row, col, 0, static_cast<unsigned char>(dist));
-			setColor(out, width, outChannel, row, col, 1, static_cast<unsigned char>((dist - floor(dist)) * 256.0));
-			setColor(out, width, outChannel, row, col, 2, static_cast<unsigned char>(getColor(img, width, nrChannels, row, col, dataChannel)));
-			setColor(out, width, outChannel, row, col, 3, static_cast<unsigned char>(255.0));
-
+			setColor(output, outputChannel, w, row, col, 0, static_cast<unsigned char>(dist));
+			setColor(output, outputChannel, w, row, col, 1, static_cast<unsigned char>((dist - floor(dist)) * 256.0));
+			setColor(output, outputChannel, w, row, col, 2, static_cast<unsigned char>(getColor(img, c, w, row, col, rawChannel)));
+			setColor(output, outputChannel, w, row, col, 3, static_cast<unsigned char>(255.0));
 		}
-
 	}
-
+	// 保存图片
+	int success = stbi_write_png("SDF.png", w, h, outputChannel, output, w * 4);
+	if (!success) std::cout << "write fail !" << std::endl;
+	return 0;
 }
-
-int main() {
-	int width = 0, height = 0, nrChannels = 0;
-
-	const char* path = "../resources/imgs/flipper128.png";
-	unsigned char* img = stbi_load(path, &width, &height, &nrChannels, 0);
-
-	if (!img)
-	{
-		fprintf(stderr, "Failed to read image '%s' !\n", path);
-		return EXIT_FAILURE;
-	}
-	printf("Input image: %d x %d pixels, %d channels\n", width, height, nrChannels);
-
-
-	int outChannel = 4;
-	unsigned char* out = new unsigned char[width * height * outChannel * sizeof(unsigned char)];
-
-	make_distance_map(img, width, height, nrChannels, 0, out, outChannel);
-
-	stbi_write_png("./output.png", width, height, outChannel, out, width * outChannel);
-
-}
-
--- Plug-and-play/AA-8SSEDT/src/main.h
+++ Plug-and-play/AA-8SSEDT/src/main.h
@@ -1,66 +1,65 @@
-#pragma once
-
-
-typedef struct Point {
-	double dist, distx, disty, gx, gy;
-	Point() {
-		dist = distx = disty = gx = gy = 0.0;
-	}
-} Point;
-
-
-bool isValid(int row, int col, int h, int w) {
-	return row >= 0 && col >= 0 && row < h && col < w;
-}
-
-void setColor(unsigned char* data, int width, int nrChannels, int row, int col, int index, unsigned char val) {
-	data[width * nrChannels * row + nrChannels * col + index] = val;
-}
-
-int getColor(unsigned char* data, int width, int nrChannels, int row, int col, int index) {
-	return data[width * nrChannels * row + nrChannels * col + index];
-}
-
+﻿#pragma once
+#include<vector>
 #define SQRT2 1.4142136
-void computegradient(std::vector<std::vector<double>>& data, std::vector<std::vector<Point>>& grid, int height, int width)
-{
-	const int kernelWidth = 3;
-	int halfKernelWidth = kernelWidth / 2;
-	double kernel_y[kernelWidth][kernelWidth] = {
+
+int main();
+
+bool isValid(int x, int y, int n, int m) {
+	return x >= 0 && y >= 0 && x < n && y < m;
+}
+
+void computeGradient(std::vector<std::vector<double>>& data, int w, int h, std::vector<std::vector<double>>& gx, std::vector<std::vector<double>>& gy) {
+	static std::vector<std::vector<double>> kernel_y = {
 		{1, SQRT2, 1},
-		{ 0, 0, 0 },
-		{-1, -SQRT2, -1},
+		{0, 0, 0},
+		{-1, -SQRT2, -1}
 	};
-	double kernel_x[kernelWidth][kernelWidth] = {
+	static std::vector<std::vector<double>> kernel_x = {
 		{1, 0, -1},
 		{SQRT2, 0, -SQRT2},
 		{1, 0, -1}
 	};
 
-	for (int row = 0; row < height; row++) {
-		for (int col = 0; col < width; col++) {
-			double gx = 0.0, gy = 0.0;
-			if (data[row][col] <= 0.0 || data[row][col] >= 1.0) continue;
-			for (int i = -halfKernelWidth; i <= halfKernelWidth; i++) {
-				for (int j = -halfKernelWidth; j <= halfKernelWidth; j++) {
-					int new_row = row + i;
-					int new_col = col + j;
-					if (isValid(new_row, new_col, height, width)) {
-						gx += data[new_row][new_col] * kernel_x[i + halfKernelWidth][j + halfKernelWidth];
-						gy += data[new_row][new_col] * kernel_y[i + halfKernelWidth][j + halfKernelWidth];
+	int kernelWidth = 3;
+	int halfKernelWidth = kernelWidth / 2;
+	for (int row = 0; row < h; row++) {
+		for (int col = 0; col < w; col++) {
+			double val_x = 0;
+			double val_y = 0;
+			if ((data[row][col] > 0.0) && (data[row][col] < 1.0)) {
+				for (int i = -halfKernelWidth; i <= halfKernelWidth; i++) {
+					for (int j = -halfKernelWidth; j <= halfKernelWidth; j++) {
+						int new_row = i + row;
+						int new_col = j + col;
+						if (isValid(new_row, new_col, h, w)) {
+							val_x += data[new_row][new_col] * kernel_x[halfKernelWidth + i][halfKernelWidth + j];
+							val_y += data[new_row][new_col] * kernel_y[halfKernelWidth + i][halfKernelWidth + j];
+						}
 					}
 				}
 			}
-			double glength = gx * gx + gy * gy;
+			gx[row][col] = val_x;
+			gy[row][col] = val_y;
+			double glength = val_x * val_x + val_y * val_y;
 			if (glength > 0.0) {
 				glength = sqrt(glength);
-				grid[row][col].gx = gx / glength;
-				grid[row][col].gy = gy / glength;
+				gx[row][col] = gx[row][col] / glength;
+				gy[row][col] = gy[row][col] / glength;
 			}
-
 		}
 	}
 }
+
+
+typedef struct Point {
+	short distx, disty;
+	double dist;
+	struct Point(int _dx, int _dy, int _dist) : distx(_dx), disty(_dy), dist(_dist) {}
+
+	double dist2() {
+		return std::sqrt(static_cast<double>(distx * distx + disty * disty));
+	}
+} Point;
 
 
 double edgedf(double gx, double gy, double a)
@@ -87,7 +86,7 @@ double edgedf(double gx, double gy, double a)
 			gx = gy;
 			gy = temp;
 		}
-		a1 = 0.5 * gy / gx;
+		a1 = 0.5 * gy / gx;  // ����ȷ����df����ʽ(4)
 		if (a < a1) { // 0 <= a < a1
 			df = 0.5 * (gx + gy) - sqrt(2.0 * gx * gy * a);
 		}
@@ -102,21 +101,26 @@ double edgedf(double gx, double gy, double a)
 }
 
 
-double distaa3(std::vector<std::vector<double>>& data, std::vector<std::vector<Point>>& grid, int closestx, int closesty, int newdistx, int newdisty) {
-	double di, df, dx, dy, gx, gy, a;
+double distaa3(std::vector<std::vector<double>>& data,
+	std::vector<std::vector<double>>& gximg,
+	std::vector<std::vector<double>>& gyimg,
+	std::vector<std::vector<Point>>& grid,
+	int closestx, int closesty, int xi, int yi)
+{
 
-
-	a = data[closestx][closesty];    // Grayscale value at the edge pixel
-	gx = grid[closestx][closesty].gx; // X gradient component at the edge pixel
-	gy = grid[closestx][closesty].gy; // Y gradient component at the edge pixel
+	double a = data[closestx][closesty];   // Grayscale value at the edge pixel
+	double gx = gximg[closestx][closesty]; // X gradient component at the edge pixel
+	double gy = gyimg[closestx][closesty]; // Y gradient component at the edge pixel
 
 	if (a > 1.0) a = 1.0;
 	if (a < 0.0) a = 0.0; // Clip grayscale values outside the range [0,1]
 	if (a == 0.0) return 1000000.0; // Not an object pixel, return "very far" ("don't know yet")
+	double df;
 
-	dx = (double)newdistx;
-	dy = (double)newdisty;
-	di = sqrt(dx * dx + dy * dy); // Length of integer vector, like a traditional EDT
+	double dx = (double)xi;
+	double dy = (double)yi;
+
+	double di = sqrt(dx * dx + dy * dy); // Length of integer vector, like a traditional EDT
 	if (di == 0) { // Use local gradient only at edges
 		// Estimate based on local gradient only
 		df = edgedf(gx, gy, a);
@@ -126,4 +130,90 @@ double distaa3(std::vector<std::vector<double>>& data, std::vector<std::vector<P
 		df = edgedf(dx, dy, a);
 	}
 	return di + df; // Same metric as edtaa2, except at edges (where di=0)
+}
+
+//#define DISTAA(row, col) (distaa3(data, gx, gy, grid, row, col))
+void Compare(std::vector<std::vector<double>>& data,
+	std::vector<std::vector<Point>>& dist,
+	std::vector<std::vector<double>>& gx,
+	std::vector<std::vector<double>>& gy,
+	int row, int col, int& h, int& w, int offsetx, int offsety, bool& changed) {
+	double epsilon = 1e-3; // Safeguard against errors due to limited precision
+	if (dist[row][col].dist <= 0) return;
+
+	int new_row = row + offsetx, new_col = col + offsety;  // Index of candidate for testing  
+	if (isValid(new_row, new_col, h, w)) {
+		int cdistx = dist[new_row][new_col].distx;     // ��ȡ�����
+		int cdisty = dist[new_row][new_col].disty;	   // ��ȡ�����
+		int newdistx = cdistx - offsetx;			   // ����������offset��
+		int newdisty = cdisty - offsety;			   // ����������offset��
+		int closestx = new_row - cdistx;
+		int closesty = new_col - cdisty;
+		double newdist = distaa3(data, gx, gy, dist, closestx, closesty, newdistx, newdisty);
+		double olddist = dist[row][col].dist;
+
+		if (newdist < olddist - epsilon) {
+			dist[row][col].dist = newdist;
+			dist[row][col].distx = newdistx;
+			dist[row][col].disty = newdisty;
+			changed = true;
+		}
+	}
+}
+
+void edtaa3(std::vector<std::vector<double>>& data,
+	std::vector<std::vector<double>>& gx,
+	std::vector<std::vector<double>>& gy,
+	std::vector<std::vector<Point>>& dist,
+	int w, int h)
+{
+	/* Initialize the distance images */
+	for (int row = 0; row < h; row++) {
+		for (int col = 0; col < w; col++) {
+			dist[row][col].distx = dist[row][col].disty = 0;
+			if (data[row][col] <= 0.0) {
+				dist[row][col].dist = 1000000.0; // Big value, means "not set yet"
+			}
+			else if (data[row][col] < 1.0) {
+				dist[row][col].dist = edgedf(gx[row][col], gy[row][col], data[row][col]); // Gradient-assisted estimate
+			}
+			else {  // img[i] == 1.0
+				dist[row][col].dist = 0.0; // Inside the object
+			}
+		}
+
+	}
+
+	// http://www.codersnotes.com/notes/signed-distance-fields/
+	int time = 0;
+	bool changed = false;
+	do {
+		time++;
+		changed = false;
+		for (int row = 0; row < h; row++) {
+			for (int col = 0; col < w; col++) {
+				Compare(data, dist, gx, gy, row, col, h, w, -1, -1, changed);
+				Compare(data, dist, gx, gy, row, col, h, w, -1, -0, changed);
+				Compare(data, dist, gx, gy, row, col, h, w, -1, 1, changed);
+				Compare(data, dist, gx, gy, row, col, h, w, 0, -1, changed);
+			}
+
+			for (int col = w - 1; col >= 0; col--) {
+				Compare(data, dist, gx, gy, row, col, h, w, 1, 0, changed);
+			}
+		}
+
+		for (int row = h - 1; row >= 0; row--) {
+			for (int col = w - 1; col >= 0; col--) {
+				Compare(data, dist, gx, gy, row, col, h, w, 0, 1, changed);
+				Compare(data, dist, gx, gy, row, col, h, w, 1, -1, changed);
+				Compare(data, dist, gx, gy, row, col, h, w, 1, 0, changed);
+				Compare(data, dist, gx, gy, row, col, h, w, 1, 1, changed);
+			}
+
+			for (int col = 0; col < w; col++) {
+				Compare(data, dist, gx, gy, row, col, h, w, 0, -1, changed);
+			}
+		}
+	} while (changed);
 }
